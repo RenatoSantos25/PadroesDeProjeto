9fd6638d38d150953304799f175d6a92b1ddc8c7
RenatoSantos25 <renatosantosfilho2005@gmail.com>
2025-05-14 14:44:25 -0300

    :recycle: Finalizando diagrama de classes da Model e Service

--- Backend/obj/Debug/net8.0/PadroesDeProjeto.AssemblyInfo.cs
+++ Backend/obj/Debug/net8.0/PadroesDeProjeto.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PadroesDeProjeto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1b30721c22e6cd7f31e0f2b5a74a7afa1dbd91a0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4851e3d79cd614337b1d34e9861ea23f7b99a948")]
 [assembly: System.Reflection.AssemblyProductAttribute("PadroesDeProjeto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PadroesDeProjeto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
